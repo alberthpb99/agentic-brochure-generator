28a6601780e131b2d82a4367b8746bf07ea14c30
alberthpb99 <alberth.pinco@correounivalle.edu.co>
2026-09-14 19:25:18 -0500

    Agregando valores por default a los inputs de la app

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from agent import create_brochure
 st.title('Generador de Folletos Corporativos con IA')
 st.caption("**Nota:** Para optimizar la latencia y el consumo de tokens en esta demo, el folleto se genera en un formato Markdown ligero de hasta 200 palabras por sección.")
 
-company = st.text_input('Nombre de la Empresa')
-url = st.text_input('URL del sitio web')
+company = st.text_input('Nombre de la Empresa', value='Anthropic')
+url = st.text_input('URL del sitio web', value='https://www.anthropic.com')
 
 if st.button('Generar Folleto', width='stretch'):
     with st.spinner('Analizando sitio web y generando folleto...'):
